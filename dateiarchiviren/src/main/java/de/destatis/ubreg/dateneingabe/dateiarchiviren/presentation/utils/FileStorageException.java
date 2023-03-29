928a23a0e6dcc2933b3a35bc4c8c69a47861ba5b
nabilsaidi10 <89589029+nabilsaidi10@users.noreply.github.com>
2023-03-29 14:55:33 +0200

    Add files via upload

---
+++ dateiarchiviren/src/main/java/de/destatis/ubreg/dateneingabe/dateiarchiviren/presentation/utils/FileStorageException.java
@@ -0,0 +1,11 @@
+package de.destatis.ubreg.dateneingabe.dateiarchiviren.presentation.utils;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ dateiarchiviren/src/test/java/de/destatis/ubreg/dateneingabe/dateiarchiviren/DateiarchivirenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.destatis.ubreg.dateneingabe.dateiarchiviren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateiarchivirenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
