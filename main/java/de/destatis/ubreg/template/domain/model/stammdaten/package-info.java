500de15b4fdef7463b0e27d44d4c6b31749a8ea3
nabilsaidi10 <89589029+nabilsaidi10@users.noreply.github.com>
2023-05-06 11:14:35 +0200

    Delete package-info.java

--- main/java/de/destatis/ubreg/template/domain/model/stammdaten/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2022 Statistisches Bundesamt
- */
-/**
- * The package contains the artifacts of the meldung module.
- *
- * @author cosma
- */
-package de.destatis.ubreg.template.domain.model.stammdaten;
