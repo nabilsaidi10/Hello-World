7ee9d4103db69964a211ea3dab39c0e97536f1e0
nabilsaidi10 <89589029+nabilsaidi10@users.noreply.github.com>
2023-05-06 11:15:36 +0200

    Delete package-info.java

--- main/java/de/destatis/ubreg/template/domain/model/land/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2022 Statistisches Bundesamt
- */
-/**
- * The package contains the artifacts of the land module.
- *
- * @author cosma
- */
-package de.destatis.ubreg.template.domain.model.land;
