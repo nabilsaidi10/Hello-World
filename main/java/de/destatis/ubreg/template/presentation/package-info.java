b13d11d2770590896d5d7e864b82eab8fa2cab0f
nabilsaidi10 <89589029+nabilsaidi10@users.noreply.github.com>
2023-05-06 11:11:34 +0200

    Delete package-info.java

--- main/java/de/destatis/ubreg/template/presentation/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2022 Statistisches Bundesamt
- */
-/**
- * The package contains the modules of the presentation layer.
- *
- * @author cosma
- */
-package de.destatis.ubreg.template.presentation;
